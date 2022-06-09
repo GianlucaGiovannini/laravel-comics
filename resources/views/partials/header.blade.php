44424f5634434425589bd192f095e39679e3bcdb
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-06-09 15:58:23 +0200

    add header, add Jumbotron

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+    <nav>
+        <div class="container">
+            <div class="row align-items-center gy-4">
+                <div class="col-12 col-lg-2 col-xl-3 text-center pt-2  text-lg-start p-lg-0">
+                    <img src="{{asset('img/dc-logo.png')}}" alt="logo DC Comics">
+                </div>
+                <div class="col-12 col-lg-10 col-xl-9 d-flex justify-content-end flex-wrap gap-3">
+                    <a href="#" class="text-uppercase">
+                        character
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        comics
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        movies
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        tv
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        games
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        collectibles
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        videos
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        fan
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        news
+                    </a>
+                    <a href="#" class="text-uppercase">
+                        shop
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/partials/jumbotron.blade.php
@@ -0,0 +1,3 @@
+<div class="jumbo_section">
+
+</div>
