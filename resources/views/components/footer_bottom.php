21dc73889e7795870baf30bc02278e790f528f0d
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-06-10 01:50:46 +0200

    test partials + components test

---
+++ resources/views/components/footer_bottom.php
@@ -0,0 +1,27 @@
+<div class="footer_bottom">
+    <div class="container py-3">
+        <div class="row row-cols-2 justify-content-around">
+            <div class="col">
+                <button class="btn_footer text-uppercase">Sign-up now!</button>
+            </div>
+            <div class="col d-flex gap-3 justify-content-end align-items-center">
+                <div class="text-uppercase follow_us">Follow us</div>
+                <a href="#">
+                    <img src="{{asset('img/footer-facebook.png')}}">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-twitter.png')}}">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-youtube.png')}}">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-pinterest.png')}}">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-periscope.png')}}">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/footer_top.blade.php
@@ -0,0 +1,122 @@
+<div class="footer_top position-relative">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2">
+            <div class="col footer_list py-4">
+
+                <div class="row row-cols-3">
+
+                    <div class="col">
+                        <h4 class="text-white py-2">DC COMICS</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                            <li>
+                                <a href="#">Movies</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div class="col">
+                        <h4 class="text-white py-2">DC</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Terms of Use</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy policy (New)</a>
+                            </li>
+                            <li>
+                                <a href="#">Ad Choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshops</a>
+                            </li>
+                            <li>
+                                <a href="#">CPSC Certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">Ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div class="col">
+                        <h4 class="text-white py-2">SITES</h4>
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                            </li>
+                            <li>
+                                <a href="#">MAD Magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Kids</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Universe</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div class="col">
+                        <h4 class="text-white py-2 m_t_negativo">SHOP</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Shop DC</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop DC Collectibles</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="copyright pb-4">
+            <p class="m-0">
+                All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise
+                <a class="cookies" href="#">noted here</a>.
+                All right Reserved
+            </p>
+            <a class="cookies" href="#">Cookies Settings</a>
+        </div>
+    </div>
+</div>
