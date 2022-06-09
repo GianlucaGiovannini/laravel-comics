82e384d2e42fc96feb9bc21031498123130f669a
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-06-09 18:57:49 +0200

    Add input on header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,18 @@
 <header>
+
+    <div class="header_banner">
+        <div class="container d-flex justify-content-end py-2">
+            <a href="#">DC power visa&reg;</a>
+            <a href="#">Additional dc sites</a>
+        </div>
+    </div>
     <nav>
         <div class="container">
             <div class="row align-items-center gy-4">
                 <div class="col-12 col-lg-2 col-xl-3 text-center pt-2  text-lg-start p-lg-0">
                     <img src="{{asset('img/dc-logo.png')}}" alt="logo DC Comics">
                 </div>
-                <div class="col-12 col-lg-10 col-xl-9 d-flex justify-content-end flex-wrap gap-3">
+                <div class="col-12 col-lg-10 col-xl-9 d-flex justify-content-end align-items-center flex-wrap gap-3 p-relative">
                     <a href="#" class="text-uppercase">
                         character
                     </a>
@@ -36,6 +43,8 @@
                     <a href="#" class="text-uppercase">
                         shop
                     </a>
+                    <input type="text" placeholder="Search">
+                    <img class="search-img" src="{{asset('img/search.png')}}">
                 </div>
             </div>
         </div>
